8d3ca4e347bc9ad545478d3695eb6feb7bae68d5
zhoucan <534523978@qq.com>
2021-07-03 22:06:27 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
 }
+#app{
+  height: 100vh;
+  background:url('./static/image/bg.png') no-repeat center center ,linear-gradient(154deg,#0779ff,#1fdfff);
+  background-size: cover;
+
+}
+
 </style>
