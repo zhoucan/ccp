7cedab69132f9ef35137c4307edb434f04073e8b
zhoucan <534523978@qq.com>
2021-07-05 17:48:08 +0800

    。。

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,8 @@ export function register (data) {
   return request({
     url: '/system/registry/registry',
     method: 'post',
-    data
+    data,
+    isShowMessage: true
   })
 }
 export function login (data) {
